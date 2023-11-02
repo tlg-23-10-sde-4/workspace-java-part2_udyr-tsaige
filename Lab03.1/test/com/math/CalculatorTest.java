bd91ecef3cf10a4dd7a27354183bddd924f2b1ec
UdyrTsaige <Udyr.BarnesTsaige@tlgcohort.com>
2023-11-02 11:29:26 -0400

    Lab03.1 (junit getting started) complete

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,13 +9,53 @@
 package com.math;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+
+import org.junit.*;
 
 public class CalculatorTest {
+    // fixture: business object(s) under test
+    private Calculator calc;
 
-  @Test
-  public void testAdd() {
-    Calculator calc = new Calculator();
-    assertEquals(5, calc.add(1, 4));  // expected, actual
-  }
+    @BeforeClass
+    public static void initializeTestRun() {
+        System.out.println("initializeTestRun");
+    }
+
+    @AfterClass
+    public static void finalizeTestRun() {
+        System.out.println("finalizeTestRun");
+    }
+
+    @Before
+    public  void setUp() {
+        System.out.println("setUp");
+
+        calc = new Calculator();
+
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("tearDown");
+    }
+
+    @Test
+    public void testISEven() {
+        System.out.println("testIsEven");
+
+        assertTrue(calc.isEven(10)); // passes is the boolean condition evaluates to true
+        assertFalse(calc.isEven(11)); // passes is the boolean condition evaluates to false
+    }
+    @Test
+    public void testDivide() {
+        System.out.println("testDivide");
+
+        assertEquals(2.5, calc.divide(5,2), .001); // expected, actual, delta
+    }
+    @Test
+    public void testAdd() {
+        System.out.println("testAdd");
+
+        assertEquals(5, calc.add(1, 4)); // expected, actial
+    }
 }
