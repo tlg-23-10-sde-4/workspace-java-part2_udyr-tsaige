0ca7e9b02dfdc718e33658c289b6bc2c7b7f066a
UdyrTsaige <Udyr.BarnesTsaige@tlgcohort.com>
2023-11-08 13:35:34 -0500

    Radish sort tests with anonymous comparator<Radish>

--- Scratch/src/org/veggie/RadishSortTest.java
+++ Scratch/src/org/veggie/RadishSortTest.java
@@ -1,6 +1,7 @@
 package org.veggie;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 class RadishSortTest {
@@ -24,17 +25,42 @@ class RadishSortTest {
         dump(radishes);
         System.out.println();
 
-        System.out.println("color, via RadishColorComparator");
-        radishes.sort(new RadishColorComparator());
+        System.out.println("color, via anonymous Comparator<Radish> class");
+        //radishes.sort(new RadishColorComparator());
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return r1.getColor().compareTo(r2.getColor());
+            }
+        });
+
         dump(radishes);
         System.out.println();
 
-        System.out.println("guysOnTop, via RadishGuysOnTopComparator");
-        radishes.sort(new RadishGuysOnTopComparator());
+        System.out.println("guysOnTop, via anonymous class");
+        // radishes.sort(new RadishGuysOnTopComparator());
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Integer.compare(r1.getGuysOnTop(), r2.getGuysOnTop());
+            }
+        });
         dump(radishes);
         System.out.println();
 
-    }
+        System.out.println("sort by tailLength, via anonymous class");
+        radishes.sort(new Comparator<Radish>() {
+
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Double.compare(r1.getTailLength(),r2.getTailLength());
+            }
+        });
+        dump(radishes);
+        System.out.println();
+
+        }
+
 
     private static void dump(List<Radish> radishes) {
         for (Radish radish : radishes) {
