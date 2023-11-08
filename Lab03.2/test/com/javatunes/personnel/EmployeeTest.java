adabca20293e7961d68ba819f1c384284ecbb1d3
UdyrTsaige <Udyr.BarnesTsaige@tlgcohort.com>
2023-11-08 15:18:22 -0500

    EmployeeTest with anonymous subclass

--- Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
+++ Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
@@ -13,8 +13,19 @@ public class EmployeeTest {
 
     @Before
     public void setUp() {
-        emp1 = new SalariedEmployee("Mary Lou", Date.valueOf("1999-09-09"));
-        emp2 = new SalariedEmployee("Mary Lou", Date.valueOf("1999-09-09"));
+       // emp1 = new DummyEmployee("Mary Lou", Date.valueOf("1999-09-09"));
+       // emp2 = new DummyEmployee("Mary Lou", Date.valueOf("1999-09-09"));
+
+        emp1 = getEmployee();
+        emp2 = getEmployee();
+
+    }
+
+    private Employee getEmployee() {
+        return new Employee("Mary Lou", Date.valueOf("1999-09-09")) {
+            public double pay() {return 0;}
+            public double payTaxes() {return 0;}
+        };
     }
 
     @Test
@@ -32,4 +43,16 @@ public class EmployeeTest {
     public void equals_shouldReturnTrue_allPropertiesSame() {
         assertEquals(emp1, emp2);
     }
+
+    // NAMED, MEMBER - LEVEL INNER CLASS
+    private class DummyEmployee extends Employee {
+        
+        public DummyEmployee(String name, Date hireDate) {
+            super(name, hireDate);
+        }
+
+        public double pay() {return 0;}
+        public double payTaxes() {return 0;}
+    }
+
 }
