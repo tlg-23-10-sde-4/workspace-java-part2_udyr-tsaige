0957888998cdf451a1ac8e23150a6961139e040d
UdyrTsaige <Udyr.BarnesTsaige@tlgcohort.com>
2023-11-13 11:10:54 -0500

    Fastjava annotation

---
+++ Lab07.2/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.fastjava.annotation","c":"Fast","l":"boost()"},{"p":"com.fastjava.annotation","c":"Fast","l":"description()"},{"p":"com.fastjava.annotation","c":"Fast","l":"log()"}]
